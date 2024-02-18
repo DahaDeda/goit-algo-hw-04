be9d5b0982339d2c645e4ab53f70a519bbbe0f9b
DahaDeda <146961908+DahaDeda@users.noreply.github.com>
2024-02-18 02:14:47 +0200

    2

--- hw4_2.py
+++ hw4_2.py
@@ -2,27 +2,24 @@ from pathlib import Path
 import re
 
 def get_cats_info():
-    with open("D:\\Projects/new_file.txt", encoding = "utf-8") as f_cat:
-        lines = f_cat.read()
-        # print(lines)
-        lines = re.sub(r"\n", ",", lines)
-        cat_list = []
-        for cat in lines.split(','):
-            # print(cat)
-            if cat.isalpha():
-                cat_name = {"name":cat}
-                cat_list.append(cat_name)
-                # print(cat_age)
-            elif cat.isdigit():
-                cat_age = {"age":cat}
-                cat_list.append(cat_age)
-                # print(cat_name)
-            else:
-                cat_id = {"id":cat}
-                # print(cat_id)
-                cat_list.append(cat_id)
-                
-        revers_cat_list = list(reversed(cat_list))
-        print(revers_cat_list)
+    try:
+        with open("D:\\Projects/new_file.txt", encoding = "utf-8") as f_cat:
+            lines = f_cat.read()
+            lines = re.sub(r"\n", ",", lines)
+            cat_list = []
+            for cat in lines.split(','):
+                if cat.isalpha():
+                    cat_name = {"name":cat}
+                    cat_list.append(cat_name)
+                elif cat.isdigit():
+                    cat_age = {"age":cat}
+                    cat_list.append(cat_age)
+                else:
+                    cat_id = {"id":cat}
+                    cat_list.append(cat_id)  
+            revers_cat_list = list(reversed(cat_list))
+            print(revers_cat_list)
+    except FileNotFoundError:
+        print("Your file not found")
             
 get_cats_info()
